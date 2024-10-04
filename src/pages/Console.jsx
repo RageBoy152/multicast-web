700e330546c9178f66adaec837cf23b6d8019f81
rage <matthewm.uk@gmail.com>
2024-10-04 01:03:34 +0100

    fixed navbar component import on console.jsx

--- src/pages/Console.jsx
+++ src/pages/Console.jsx
@@ -6,7 +6,7 @@ import useUserData from '../hooks/useUserData';
 //  React Compontents
 import { FeedListSideBar } from "../components/FeedListSideBar";
 import { OutputList } from "../components/OutputList";
-import { NavBar } from "../components/navBar";
+import { NavBar } from "../components/NavBar";
 import { ContextMenu } from '../components/ContextMenu';
 import { FeedModal } from '../components/FeedModal';
 import { useState } from 'react';
