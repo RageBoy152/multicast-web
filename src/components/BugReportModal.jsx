dfa0deeabeced82601183ddb5db3dcb9a2c886e9
rage <matthewm.uk@gmail.com>
2024-10-05 20:41:55 +0100

    finish bug report form

--- src/components/BugReportModal.jsx
+++ src/components/BugReportModal.jsx
@@ -39,18 +39,18 @@ export function BugReportModal() {
         </div>
         <form className="flex w-11/12 flex-col gap-5 py-4" id='bug-report-form' name='bug-report-form' method='POST' action='/'>
           <input type="hidden" name="form-name" value="bug-report-form" />
-          
+
           <div className="flex flex-col gap-1">
             <label htmlFor="bug-description">Brief or detailed description</label>
             <textarea name="bug-description" id="bug-description" className='bg-secondary px-2 py-1 resize-none h-[120px]'></textarea>
           </div>
 
           <div className="flex flex-col gap-1">
-            <label htmlFor="screenshot">Screenshot (optional)</label>
-            <input type="file" name="screenshot" id="screenshot" className='bg-secondary hover:bg-secondary/80 cursor-pointer p-2' />
+            <label htmlFor="contact-info">How can we get back to you? (optional)</label>
+            <input type="text" name="contact-info" id="contact-info" className='bg-secondary p-2' />
           </div>
 
-          <div className="flex flex-col gap-1">
+          <div className="flex-col gap-1 hidden">
             <label htmlFor="console">Console (automated)</label>
             <input type="text" name="console" id="console" className='bg-secondary p-2' value={JSON.stringify(consoleValue)} />
           </div>
