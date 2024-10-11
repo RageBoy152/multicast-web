5edf7e57079d95489a89dc72100cf07245a91865
rage <matthewm.uk@gmail.com>
2024-10-11 09:39:20 +0100

    changed forced noti notificationId

--- src/hooks/useUserData.js
+++ src/hooks/useUserData.js
@@ -125,7 +125,7 @@ const defaultConfig = {
 
 
 export const forcedNotif = {
-  "notificationId": "f0f326f4-f781-4407-a571-751670a765471",
+  "notificationId": "f0f326f4-f781-4407-a571-751670a765472",
   "timestamp": new Date().toISOString(),
   "title": "New update - MultiCast V2.0.2",
   "body": "Read the release notes <a href='https://github.com/RageBoy152/multicast-web/releases/tag/2.0.2' target='_blank' class='text-text-shade hover:text-text underline'>here</a>",
